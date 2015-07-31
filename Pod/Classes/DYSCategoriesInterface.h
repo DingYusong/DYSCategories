a2bcf860efd60cbeb886d8659526c62a8c247b3b
丁玉松 <dys90@qq.com>
2015-07-31 12:39:35 +0800

    添加私有库接口文件。

---
+++ Pod/Classes/DYSCategoriesInterface.h
@@ -0,0 +1,19 @@
+//
+//  DYSCategoriesInterface.h
+//  Pods
+//
+//  Created by 丁玉松 on 15/7/31.
+//
+//
+
+#ifndef Pods_DYSCategoriesInterface_h
+#define Pods_DYSCategoriesInterface_h
+
+#import "NSArray+Safe.h"
+#import "NSDictionary+Safe.h"
+#import "NSString+DYSFormat.h"
+#import "UIColor+RGBValue.h"
+#import "UIImageView+RoundCorner.h"
+#import "UIViewExt.h"
+
+#endif
